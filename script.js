f5df9594f9ca0709920d385fa6c33932cda5f885
Matan <0aroundtheworld1@gmail.com>
2022-02-12 16:43:40 +0200

    added divs and css design

---
+++ script.js
@@ -0,0 +1,9 @@
+const display = document.getElementById('calscreen');
+
+// TO DO
+//take care of style - arrange buttons in order
+//more style - take care of screen, make sure it looks good
+//more style - take care of general calculator design
+//JS - link every button to it's function
+//JS - take care of bugs, as is functions that interfere with each other
+
